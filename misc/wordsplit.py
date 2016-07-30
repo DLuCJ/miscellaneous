3a01317988510f13027274119ae11f184623b8b0
dlu <dizzylu@gmail.com>
2016-07-29 22:47:07 -0700

    TODO

---
+++ misc/wordsplit.py
@@ -0,0 +1,12 @@
+dictionary = ["nobody", "knows", "the", "troubles", "I",
+         "have", "seen", "but", "jesus", "and",
+         "these", "are", "some", "filler", "words"]
+
+squish = "nobodyknowsthetroublesihaveseennobodyknowsbutjesus"
+
+
+def unsquish(squish):
+    print("SQUISH")
+
+
+unsquish(squish)
