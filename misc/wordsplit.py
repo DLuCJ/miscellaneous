1a4007b1429ac06c0d3630db4405991bb7243bea
dlu <dizzylu@gmail.com>
2016-07-31 17:05:34 -0700

    try 1

--- misc/wordsplit.py
+++ misc/wordsplit.py
@@ -1,12 +1,32 @@
-dictionary = ["nobody", "knows", "the", "troubles", "I",
-         "have", "seen", "but", "jesus", "and",
-         "these", "are", "some", "filler", "words"]
+dictionary = ["nobody", "knows", "the", "troubles", "i've", 
+              "seen", "but", "jesus", "and", "these", "are", 
+              "some", "filler", "words"]
 
-squish = "nobodyknowsthetroublesihaveseennobodyknowsbutjesus"
+unsquished = []
+squished = "nobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusohthehumanity"
+squishlen = len(squished)
+
+#def unsquish(squished, unsquished, slicepos):
+#    if slicepos == 0:
+#        return unsquished
+#
+#    if squished[:slicepos] in dictionary:
+#        unsquished.append(squished[:slicepos])
+#        return unsquish(squished[slicepos:], unsquished, squishlen)
+#
+#    return unsquish(squished, unsquished, slicepos - 1)
 
 
-def unsquish(squish):
-    print("SQUISH")
+def unsquish(squished, unsquished):
+
+    for i in range(squishlen, 0, -1):
+        if squished[:i] in dictionary:
+            unsquished.append(squished[:i])
+            unsquish(squished[i:], unsquished)
+            break
+            
+    return unsquished
 
 
-unsquish(squish)
+splitwords = unsquish(squished, unsquished)
+print(splitwords)
