7b7e55dd399e8cab55f46a8188b45efbf739eb95
dlu <dizzylu@gmail.com>
2016-07-31 18:17:20 -0700

    bug: appends dup of last match, but handles misspellings a bit better

--- misc/wordsplit.py
+++ misc/wordsplit.py
@@ -3,7 +3,7 @@ dictionary = ["nobody", "knows", "the", "troubles", "i've",
               "some", "filler", "words"]
 
 unsquished = []
-squished = "nobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusnobodyknowsthetroublesi'veseennobodyknowsbutjesusohthehumanity"
+squished = "nobodyknowstheTroublesi'veseennobodyknowsbutjesus"
 squishlen = len(squished)
 
 #def unsquish(squished, unsquished, slicepos):
@@ -17,16 +17,40 @@ squishlen = len(squished)
 #    return unsquish(squished, unsquished, slicepos - 1)
 
 
-def unsquish(squished, unsquished):
-
+def unsquish(squished, unsquished, slicepos):
     for i in range(squishlen, 0, -1):
         if squished[:i] in dictionary:
             unsquished.append(squished[:i])
-            unsquish(squished[i:], unsquished)
+            unsquish(squished[i:], unsquished, i)
             break
-            
+
     return unsquished
 
 
-splitwords = unsquish(squished, unsquished)
-print(splitwords)
+lo = 0
+prevsz = -1
+squishtemp = squished
+
+while True:
+    sz = 0
+    
+    unsquished = unsquish(squishtemp, unsquished, lo)
+    words = [word for word in unsquished]
+
+    for word in words:
+        sz = sz + len(word)
+
+    if sz == squishlen or lo == squishlen:
+        break
+
+    if prevsz >= 0 and sz == prevsz:
+        lo = lo + 1
+    else:
+        lo = sz + 1
+
+    prevsz = sz
+
+    squishtemp = squished[lo:]
+
+print(unsquished)
+
